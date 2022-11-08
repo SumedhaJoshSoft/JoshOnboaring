f69cbd9b56f33d7a5c580b5534019394f702f2ef
SumedhaJoshSoft <sumedha.jagtap@joshsoftware.com>
2022-11-08 15:33:47 +0530

    first commit

---
+++ try1.go
@@ -0,0 +1,26 @@
+
+package main
+
+
+
+import "fmt"
+
+
+
+const PI float64
+
+
+
+func main() {
+
+    PI = 3.14
+
+    radius := 15.00
+
+    area := radius * radius * PI
+
+
+
+    fmt.Printf("The area of a circle is %v\n", area)
+
+}
